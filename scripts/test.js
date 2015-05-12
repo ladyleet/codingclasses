447ca2a3405d425d8096dae8b3590725e5a44533
Tracy Lee <tracy@wingman.events>
2015-05-11 18:39:03 -0700

    day-8-js

---
+++ scripts/test.js
@@ -0,0 +1,94 @@
+// variable declaration 
+var x;
+
+// variable assignment 
+// variable initialization 
+// giving x a value 
+x=5;
+// an example of declaring and initializing a variable on the same line
+// var y = "words";
+
+// alert("welcome to javascript land");
+// var name = prompt("please enter your name", "default name");
+
+// // you can capture values
+// var response = confirm("do you want to launch all the weapons?");
+
+// console.log("worlds, words, titanic");
+
+// alert(x);
+
+// parentheses, exponents, multiplication, divide, addition, subtraction
+
+// var sum = 6+7;
+
+// goal is to flip values of x and y
+// var a = 5;
+// var b = 17;
+
+// a = (5*17)/5;
+// b = (17*5)/17;
+
+// alert(a + "yay" + b);
+
+// var x = 5;
+// var y = 15;
+
+// y = [x,x=y][0]
+
+var input = prompt("what is today's weekday?");
+switch(input.toLowerCase()){
+	case "monday":
+		window.location="http://facebook.com";
+		break;
+	case "tuesday":
+		alert("Today is Wednesday! Thank you!");
+		break;
+	case "wednesday":
+		alert("Today is Hump day! Thank you!");
+		break;
+	default :
+	alert("I am sorry, I didn't quite catch that");
+	break;
+}
+
+
+// var checkNumber = parseInt(input);
+// if(checkNumber > 0){
+// 	alert("You are " + checkNumber + " years old!")
+// }
+// else if (typeof checkNumber !== "number" || "checkNumber === NaN"){
+// 	alert("error");
+// }
+
+
+
+// if (isNaN(checkNumber)){
+// 	alert("error");
+// }
+
+// else if (checkNumber > 0){
+// 	alert ("You are" + checkNumber + " years old!");
+// }
+// else {
+// 	alert ("catchall");
+// alert("This will show you what your married last name will look like!")
+// var user = prompt("What is your last name?");
+// var user1 = prompt("What is his last name?");
+// alert (user + "-" + user1);
+
+// var user2 = prompt("number 1");
+// var user3 = prompt("number 2");
+// alert(parseInt(user2) + parseInt(user3) +"!");
+
+
+
+
+
+
+
+
+
+
+
+
