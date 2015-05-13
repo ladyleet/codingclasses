d9cc9e32a4777f2c13e4e8204768b1961e7d4b61
Tracy Lee <tracy@wingman.events>
2015-05-13 13:45:12 -0700

    js-day-3

---
+++ scripts/day-3-1-js.js
@@ -0,0 +1,23 @@
+var wayBackWhen = new Date(5);
+console.log(wayBackWhen);
+
+// var monthWords =["Jan","Feb","May","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+
+// var date = new Date();
+// var month = date.getMonth();
+// var newMonth = monthWords[month];
+// console.log("This month is "+newMonth);
+
+
+// [0,1,2,3,4,5,6,7,8,9,10,11,12]
+// 0 === Jan
+// 1 ===
+// if (month.["dog", "doggies", "doggy", "puppy", "dogs"].indexOf(answer.toLowerCase()) >=0)
+
+// var date = new Date();
+// console.log(date);
+// var month = date.getMonth();
+// console.log(month);
+// var weekDay = date.getDay();
+// console.log(weekDay);
+// console.log(date, month, weekDay);
